d617d2259fec450ad6a76d8d6517ea727817d176
Mayeda Islam <mayedakonika@gmail.com>
2026-09-17 15:23:10 +0600

    exploring tuples

---
+++ src/tuples.ts
@@ -0,0 +1,3 @@
+const exampleTuple: [string, number, boolean] = ["Hello", 42, true];
+console.log(exampleTuple); // Output: ["Hello", 42, true]
+//tuple is a special type of array that can contain elements of different types, but the order and types of the elements are fixed. It is better to use tuple instead of array when you want to have a fixed number of elements with different types.
